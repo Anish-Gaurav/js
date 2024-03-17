60d45ab1729ff255f71f77f0d3fdf02eb8048c9b
Anish-Gaurav <154647172+Anish-Gaurav@users.noreply.github.com>
2024-03-17 07:51:50 +0000

    Pending changes exported from your codespace

---
+++ Basics/datatypes1.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as a newer version
+
+//alert(3+3) // we are using nodeJs,not browser 
+
+let name = "ANISH"
+let age ="20"
+let isLoggedIn = false
+let State ;
+
+/*
+number     2 to the power 53
+bigint
+string ""
+boolean true/false
+null  standaone value
+undefined
+symbol (mainly implemented in react) for uniqueness
+objects
+*/
+
+console.log(typeof age) ;
+console.log(typeof null) ; //an object in javaScript
+console.log(typeof undefined) ; //undefined is type
